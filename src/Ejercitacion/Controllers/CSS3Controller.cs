14456675f011bcc568a123e8cce32a1df5cf72e4
Alexis <alexisrk5@gmail.com>
2020-05-10 22:56:15 -0300

    .- First Excersise of CSS (Como se usa 1).

--- src/Ejercitacion/Controllers/CSS3Controller.cs
+++ src/Ejercitacion/Controllers/CSS3Controller.cs
@@ -6,12 +6,41 @@ using System.Web.Mvc;
 
 namespace Ejercitacion.Controllers
 {
-    public class CSS3Controller : Controller
-    {
-        // GET: CSS3
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+	public class CSS3Controller : Controller
+	{
+		// GET: CSS3
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		public ActionResult ComoSeUsa1()
+		{
+			return View();
+		}
+		public ActionResult ComoSeUsa2()
+		{
+			return View();
+		}
+		public ActionResult ComoSeUsa3()
+		{
+			return View();
+		}
+		public ActionResult Selectores1()
+		{
+			return View();
+		}
+		public ActionResult Selectores2()
+		{
+			return View();
+		}
+		public ActionResult Selectores3()
+		{
+			return View();
+		}
+		public ActionResult MaquetaciónDeDiseñoElastico()
+		{
+			return View();
+		}
+	}
 }
